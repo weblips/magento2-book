9d61a5c52984a4be37c7f4b111749aa12a5ce895
WebLips <weblips@gmail.com>
2017-03-15 09:27:55 +0200

    add module Weblips_Core && Weblips_Logo

---
+++ app/code/Weblips/Core/Controller/Adminhtml/Index/Index.php
@@ -0,0 +1,32 @@
+<?php
+ 
+namespace Weblips\Core\Controller\Adminhtml\Index;
+ 
+use Magento\Framework\App\Action\Context;
+ 
+class Index extends \Magento\Framework\App\Action\Action
+{
+    protected $_resultPageFactory;
+ 
+    public function __construct(Context $context, \Magento\Framework\View\Result\PageFactory $resultPageFactory)
+    {
+        $this->_resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+ 
+    public function execute()
+    {
+        $resultPage = $this->_resultPageFactory->create();
+        return $resultPage;
+    }
+    
+    /**
+     * Check for is allowed
+     *
+     * @return boolean
+     */
+    protected function _isAllowed()
+    {
+        return $this->_authorization->isAllowed('Weblips_Core::menu');
+    }
+}
---
+++ app/code/Weblips/Logo/Controller/Adminhtml/Bookclub/Index.php
@@ -0,0 +1,27 @@
+<?php
+ 
+namespace Weblips\Logo\Controller\Adminhtml\Bookclub;
+ 
+use Magento\Framework\App\Action\Context;
+ 
+class Index extends \Magento\Framework\App\Action\Action
+{
+    protected $_resultPageFactory;
+ 
+    public function __construct(Context $context, \Magento\Framework\View\Result\PageFactory $resultPageFactory)
+    {
+        $this->_resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+ 
+    public function execute()
+    {
+        $resultPage = $this->_resultPageFactory->create();
+        return $resultPage;
+    }
+    
+    protected function _isAllowed()
+    {
+        return true;
+    }
+}
